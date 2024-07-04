9a0db60148ef71a5974ebd9b631e90e63624a9cd
mwunnam <mwunnam@gmail.com>
2024-07-04 23:06:27 +0000

    correction on task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -6,4 +6,9 @@ import math
 
 
 def floor(n: float) -> int:
+    """
+    Function to convert float to int with floor
+    n: parameter of type float
+    Return: int
+    """
     return math.floor(n)
