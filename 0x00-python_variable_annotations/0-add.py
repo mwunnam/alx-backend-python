0d9a811c44cfbb4a61b97e9cb3e951af1fc6f91a
mwunnam <mwunnam@gmail.com>
2024-07-04 23:46:17 +0100

    done 0 to 4

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """
+    A function to add to float types
+    Returns a float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    This function convers a float to a string
+    n: parameter of type float
+    Returns a string
+    """
+    return str(n)
