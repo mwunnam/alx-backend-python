a31c243ac07885d3fa8dba517ac8f153ac0cd333
mwunnam <mwunnam@gmail.com>
2024-07-05 10:33:40 +0000

    correction

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,4 +8,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Function
     Returns a tuple with string and float
     """
-    return k, float(v ** 2)
+    return (k, float(v ** 2))
