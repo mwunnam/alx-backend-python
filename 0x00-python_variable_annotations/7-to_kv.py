0d2ac154e7af5b314702e12b9dbcb1112bc264ec
mwunnam <mwunnam@gmail.com>
2024-07-05 10:15:33 +0000

    done with task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Sting and int/float to tuple"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function
+    Returns a tuple with string and float
+    """
+    return k, float(v ** 2)
