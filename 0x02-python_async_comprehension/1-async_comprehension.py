44923e39ead0079a8bb4f9a722a7d3c968f2c5cd
mwunnam <mwunnam@gmail.com>
2024-12-12 16:20:31 +0100

    done with task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async Comprehension"""
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Getting results using async comprehension
+    """
+    results = [result async for result in async_generator()]
+    return results
