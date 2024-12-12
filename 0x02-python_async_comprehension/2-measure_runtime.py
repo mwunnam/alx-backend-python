181e501e45f6898af03e7080748972a9b195385c
mwunnam <mwunnam@gmail.com>
2024-12-12 16:46:23 +0100

    done with task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Time Run time for async compressions"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    This to measure the time taken to run 4 async_compression parallel
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.perf_counter()
+    runtime = end_time - start_time
+    return runtime
