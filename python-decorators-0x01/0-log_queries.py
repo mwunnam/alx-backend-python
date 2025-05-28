b1e4a0b86b09582a6e34b16d97460e75780d5e5c
mwunnam <mwunnam@gmail.com>
2025-05-28 22:24:49 +0000

    done with task 0

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3 
+import functools
+
+#### decorator to lof SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Extract the query args or kwargs
+        query = kwargs.get("query")
+        if not query and args:
+            query = args[0]
+
+        # Log message
+        print(f"[LOG] {func.__name__} about to execute: {query}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
