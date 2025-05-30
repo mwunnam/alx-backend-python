64401e8b09ed25164d1c6145f8df67c32abd977e
mwunnam <mwunnam@gmail.com>
2025-05-30 11:03:54 +0000

    done with task 1

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -3,13 +3,13 @@ import sqlite3
 class DatabaseConnection(object):
     def __init__(self, query, db_name):
         self.query = query
-        self.db_name = query
+        self.db_name = db_name
         self.conn = None
         self.cursor = None
 
     def __enter__(self):
         self.conn = sqlite3.connect(self.db_name)
-        self.cursor = self.conn.cursor
+        self.cursor = self.conn.cursor()
         self.cursor.execute(self.query)
         return self.cursor.fetchall()
 
@@ -17,5 +17,7 @@ class DatabaseConnection(object):
         if self.conn:
             self.conn.close()
 
-
-with DatabaseConnection("CREATE DATABASE 'users'") as
+select_table = "SELECT * FROM users"
+with DatabaseConnection(select_table, "users.db") as f:
+    for x in f:
+        print(x)
---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class ExecuteQuery():
+    def __init__(self, db_name, query, params=None, ):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.curson = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        result = self.cursor.fetchall()
+        return result 
+
+    
+    def __exit__(self, type, value, traceback):
+        if self.conn:
+            self.conn.close()
+
