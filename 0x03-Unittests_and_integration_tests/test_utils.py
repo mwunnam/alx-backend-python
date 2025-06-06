a8710e8d3aba47111e891eafd9040bc158c03e77
mwunnam <mwunnam@gmail.com>
2025-06-06 02:24:55 +0000

    done with task 4

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,12 +67,18 @@ class TestMemoize(unittest.TestCase):
         Test case for memoize
         """
         class TestClass:
+            """
+            Test case for memoize
+            """
             def a_method(self):
                 return 42
             
 
             @memoize
             def a_property(self):
+                """
+                Test case for memoize
+                """    
                 return self.a_method()
             
         with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
