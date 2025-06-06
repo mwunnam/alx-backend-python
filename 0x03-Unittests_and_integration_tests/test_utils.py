ada32ccbbd234be210ce380d4a0473b11bfe17dc
mwunnam <mwunnam@gmail.com>
2025-06-06 00:31:17 +0000

    done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,17 @@ class TestAccessNestedMap(unittest.TestCase):
         test for test_access_nested_map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+            ({}, ("a",), "'a'"),
+            ({"a": 1}, ("a", "b"), "'b'"),  
+        ])
+    
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+        """
+        Testing KeyError error Handling 
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_message)
