e7192f7dae1e12f5f05e06b6e3a2e2d4b2cc38d7
mwunnam <mwunnam@gmail.com>
2025-06-06 15:12:49 +0000

    correction

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,19 +9,18 @@ class TestGithubOrgClient(unittest.TestCase):
     Parameterize and patch as decorators using GithubOrgClient from the client module
     """
     @parameterized.expand([
-        ("google", ),
-        ("abc", ) ,
+        ("google",{"login": "google", "id": 1}),
+        ("abc", {"login": "abc", "id": 2}),
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name, expected_payload, mock_get_json):
         """
             Test case for org mocking to see how it performs
         """
-        expected_payload = {"key": "value"}
         mock_get_json.return_value = expected_payload
 
         client = GithubOrgClient(org_name)
         result = client.org()
-        self.assertEqual(result, expected_payload)
 
+        self.assertEqual(result, expected_payload)
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
