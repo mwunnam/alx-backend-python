9dd622a0f14e54c0ae4ac7009739ff31c38507be
mwunnam <mwunnam@gmail.com>
2025-06-06 23:23:32 +0000

    correctoin

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
         upload_to='profiles/',
@@ -15,18 +17,20 @@ class User(AbstractUser):
         return self.username
 
 class Conversation(models.Model):
+    conversaton_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         participant_names = ', '.join([user.username for user in self.participants.all()])
-        return f"Conversation ({self.id}): {participant_names}"
+        return f"Conversation ({id}): {participant_names}"
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
     Conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(default=timezone.now)
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(default=timezone.now)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
