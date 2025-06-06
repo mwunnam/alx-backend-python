7ea316d8a5adad7bd427578c6d57193da3d598fa
mwunnam <mwunnam@gmail.com>
2025-06-06 15:01:48 +0000

    done with task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import unittest
 from unittest.mock import patch, MagicMock
 from client import GithubOrgClient
@@ -11,11 +12,10 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", ),
         ("abc", ) ,
     ])
-
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """
-            Test case for org mocking to see how it performs         
+            Test case for org mocking to see how it performs
         """
         expected_payload = {"key": "value"}
         mock_get_json.return_value = expected_payload
