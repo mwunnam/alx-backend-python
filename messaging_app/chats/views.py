0027c37a65b9ff68370f2e94965a2fa8cffc848a
mwunnam <mwunnam@gmail.com>
2025-06-07 00:14:04 +0000

    done with task 4

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import CoversationSerializer, MessageSerializer
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+User = get_user_model()
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = CoversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+        return conversation
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get('conversation_pk')
+        return Message.objects.filter(
+            conversation_id=conversation_id,
+            conversation_id__participants=self.request.user
+        )
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get('conversation_pk')
+        conversation = get_object_or_404(Conversation, pk=conversation_id)
+
+        if self.request.user not in conversation.participants.all():
+            return Response({"detail": "Not a participant of this conversation."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        serializer.save(sender=self.request.user, conversation_id=conversation)
