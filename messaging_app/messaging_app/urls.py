e4e4faee436286b4b333e9a38647ef77a066e294
mwunnam <mwunnam@gmail.com>
2025-06-07 00:34:52 +0000

    correction

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_nested import routers
+from rest_framework.routers import DefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
