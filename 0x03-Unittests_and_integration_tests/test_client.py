215ad1718e123fe723ac8e4f03b02cc994d74aa5
mwunnam <mwunnam@gmail.com>
2025-06-10 23:47:25 +0000

    done with task 7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -60,6 +60,17 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/google/repo"
             )
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """
+        Test case for has_license returns True or false
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
